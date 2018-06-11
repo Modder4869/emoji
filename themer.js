b3a64be6f974e8238fc1112348774e655d55a561
AkaShrug <39406056+AkaShrug@users.noreply.github.com>
2018-06-11 13:39:04 +0300

    Add files via upload

--- themer.js
+++ themer.js
@@ -1,14 +1,22 @@
 var themeCSS;
 let themeName;
 let cPicker = '#colorButton';
+let cPickerTc = '#colorButtonTc';
 let cRgb = {
     r: '190',
     g: '78',
     b: '180'
 };
+let cRgbTc = {
+    r: '0',
+    g: '0',
+    b: '0'
+};
 let action;
 let bgImage = 'https://mwittrien.github.io/BetterDiscordAddons/ThemesV2/BasicBackground/background.jpg';
-
+let memberT;
+let msgT;
+let colorT;
 $(document).ready(function () {
     addCp();
     genTheme();
@@ -22,31 +30,43 @@ function addCp() {
             $(cPicker).css('background', color.toRgbString()) // #ff0000
         }
     });
+    $(cPickerTc).spectrum({
+        appendTo: '#modal',
+        move: function (color) {
+            cRgbTc = color.toRgb();
+            $(cPickerTc).css('background', color.toRgbString()) // #ff0000
+        }
+    });
 }
 
 function genTheme() {
-    themeName = document.getElementById('themeName').value;
+    themeName = document.getElementById('themeName').value ? document.getElementById('themeName').value : 'imblind_and_didnt_see_name_input';
     bgImage = document.getElementById('imageLink').value;
-
-    themeCSS = `//META{"name":"${themeName}","description":"Generated Automatically using Themer.js. Theme base made by DevilBro\","author":"themer, DevilBro","version":"1.0.4"}*//
+    themeCSS = `//META{"name":"${themeName}","description":"Generated Automatically using akashrug.github.io/theme/ Theme base made by DevilBro\","author":"themer, DevilBro","version":"1.0.4"}*//
 
 @import url(https://mwittrien.github.io/BetterDiscordAddons/Themes/BasicBackground/BasicBackground.css);
 
 :root {
-	--transparencycolor: 0,0,0;		/* default: 0,0,0 */
-	--transparencyalpha: 0.15;		/* default: 0.15 */
-	--messagetransparency: 0.5;		/* default: 0.5 (set this to 0 to get rid of the black boxes around messages) */
-	--memberlistransparency: 0;		/* default: 0 (additional darkness for member list can be changed in case a bright background makes the list hard to read) */
-	--accentcolor:${cRgb.r},${cRgb.g},${cRgb.b}; 		/* default: 190,78,180 discord: 114,137,218 bd-blue: 58,113,193 */
+	--transparencycolor: ${cRgbTc.r},${cRgbTc.g},${cRgbTc.b};		/* default: 0,0,0 */
+	--transparencyalpha: ${ta};		/* default: 0.15 */
+	--messagetransparency: ${msgT};		/* default: 0.5 */
+	--memberlistransparency: ${memberT};		/* default: 0 (additional darkness for member list can be changed in case a bright background makes the list hard to read)*/
+	--accentcolor:${cRgb.r},${cRgb.g},${cRgb.b}; 		/* default: 190,78,180 discord: 114,137,218 bd-blue: 58,113,193*/
 	--background: url("${bgImage}"); /* replace the link in url() with your own direct image link to change the background */
 	--backdrop: rgba(0,0,0,0.4);	/* default: rgba(0,0,0,0.4) can also be changed to an image like --background */
 }`
 
 }
 
-function check(action) {
+function inputValues() {
+    msgT = document.getElementById('msgt').value;
+    memberT = document.getElementById('mt').value;
+    colorT = document.getElementById('ta').value;
+}
 
-    genTheme()
+function check(action) {
+    inputValues();
+    genTheme();
     if (!(bgImage.startsWith('https:') && bgImage.match(/\.(jpeg|jpg|gif|png|webp)$/) != null)) {
         $('#warn').css('display', 'block')
         return;
@@ -59,7 +79,6 @@ function check(action) {
         element.style.display = 'none';
     } else if (action.toString() == 'preview') {
         element.setAttribute('href', `./DiscordPreview.html?url=data:text/plain;charset=utf-8${encodeURI(themeCSS)}`);
-        console.log(element);
         element.setAttribute('target', 'blank');
         element.style.display = 'none';
 
